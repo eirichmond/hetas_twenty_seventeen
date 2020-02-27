3d55bf48f567941b7779d60dbf76358d3b22271b
Elliott Richmond <elliott@squareonemd.co.uk>
2020-02-27 15:45:12 +0000

    update production

--- functions.php
+++ functions.php
@@ -318,7 +318,7 @@ include_once 'metaboxes/ta-manufacturers-spec.php';
 /**
  * Include all legacy
  */
-//require get_template_directory() . '/inc/settings.php';
+require get_template_directory() . '/inc/settings.php';
 require get_template_directory() . '/inc/legacy-code.php';
 require get_template_directory() . '/inc/required-legacy-code.php';
 
